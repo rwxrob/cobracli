83318542209fe7c3317a5bc92454b3f1ebc69b6a
Rob Muhlestein <rob@rwx.gg>
2024-09-14 11:22:01 -0400

    removing yaml dep

--- internal/yaml/doc.go
+++
@@ -1,5 +0,0 @@
-/*
-Package yaml is a helper package designed to simplify use of the [gopkg.in/yaml.v3] package and
-the [gopkg.in/yaml.v3.Node] type by codifying how a Node struct is used to represent the different collections, scalars, and aliases as defined by the YAML specification.
-*/
-package yaml
